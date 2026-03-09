ad359cd91c6ce84014d1680312375768f9a3c3e2
CenTdemeern1 <timo.herngreen@gmail.com>
2026-03-09 20:40:49 +0100

    More docs, also declare try_read

--- cruil/src/error.rs
+++ cruil/src/error.rs
@@ -2,37 +2,54 @@ use crate::keyboard::keys::raw::{KEY_ERR_POST_FAIL, KEY_ERR_UNDEFINED};
 use hidapi::HidError;
 use thiserror::Error;
 
+/// Cruil's [`Result`] type.
 pub type CruilResult<T> = Result<T, CruilError>;
 
+/// An error that indicates that something went wrong with the keyboard device, presumably a hardware fault.
+///
+/// Not quite ["printer on fire"](https://en.wikipedia.org/wiki/Lp0_on_fire), but I wish the best of luck to any users that encounter this.
 #[derive(Debug, Error, PartialEq, Eq, Hash)]
 #[repr(u8)]
 pub enum KeyboardError {
+    /// The keyboard failed to POST.
     #[error("The keyboard failed to POST")]
     POSTFail = KEY_ERR_POST_FAIL,
+    /// The keyboard ran into an unknown error.
     #[error("The keyboard ran into an unknown error")]
     Undefined = KEY_ERR_UNDEFINED,
 }
 
+/// An error that indicates that the device sent an invalid response.
 #[derive(Debug, Error, PartialEq, Eq, Hash)]
 pub enum ProtocolViolation {
+    /// The device sent a response that was too short.
     #[error("The device's response was too short ({0})")]
     ResponseTooShort(usize),
 }
 
+/// Cruil's all-encompassing error type.
+// TODO: Try to get rid of the HidApi variant and other variants that can be separated
 #[derive(Debug, Error)]
 pub enum CruilError {
+    /// Something went wrong with the keyboard device.
+    // TODO: Currently unimplemented
     #[error("Keyboard error: {0}")]
     KeyboardError(KeyboardError),
+    /// The device sent an invalid response.
     #[error("Protocol violation: {0}")]
     ProtocolViolation(ProtocolViolation),
+    /// This kind of device is unsupported and cannot be opened by cruil.
     #[error("Unsupported device ({0:#X}, {1:#X})")]
     UnsupportedDeviceKind(u16, u16),
+    /// The USB pipe stalled.
     #[error("USB pipe stalled")]
     PipeStalled,
+    /// Internal unhandled error.
     #[error("hidapi error: {0}")]
     HidApi(#[from] HidError),
 }
 
+/// An error that indicates that the given key code sent by the device is unknown and could not be parsed.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Error)]
 #[error("Unrecognized key {0:#X}")]
 pub struct UnrecognizedKey(pub u8);
--- cruil/src/readable_device.rs
+++ cruil/src/readable_device.rs
@@ -17,7 +17,7 @@ use crate::CruilResult;
 /// which will use a background thread to continuously read the device and send reports back.
 /// See its documentation for more information.
 pub trait ReadableDevice {
-    /// The type of input state associated with this type of device.
+    /// The type of the parsed input state associated with this type of device.
     type State;
 
     /// Reads a HID report, and returns the raw bytes.
@@ -28,9 +28,22 @@ pub trait ReadableDevice {
     /// [`read`](Self::read) uses this parsed data and internal state to track which keys were "just pressed"
     /// or "just released", so calling this function will make [`read`](Self::read) miss this frame and possibly drop the input.
     ///
+    /// If no report is available, the behavior depends on the value of `blocking`:
+    /// - If `blocking` is `true`: Blocks and waits for the next report, then returns that.
+    /// - If `blocking` is `false`: Returns an empty slice.
+    ///
     /// The returned slice is a reference to the internal buffer.
     fn read_raw(&mut self, blocking: bool) -> CruilResult<&[u8]>;
 
+    /// Reads and parses a HID report, and returns it, if one is available.
+    ///
+    /// This method does not block and returns [`None`] if no report is available.
+    fn try_read(&mut self) -> Option<CruilResult<Self::State>>;
+
     /// Reads and parses a HID report, and returns it.
+    ///
+    /// If no report is available, the behavior depends on the value of `blocking`:
+    /// - If `blocking` is `true`: Blocks and waits for the next report, then parses and returns that.
+    /// - If `blocking` is `false`: Creates a "ghost report" that reports the state as unchanged from the previous report.
     fn read(&mut self, blocking: bool) -> CruilResult<Self::State>;
 }
