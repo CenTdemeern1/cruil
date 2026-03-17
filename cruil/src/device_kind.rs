acd33fb592c2218f48a94c337d953465b93d75c1
CenTdemeern1 <timo.herngreen@gmail.com>
2026-03-17 04:16:13 +0100

    This isn't gonna work

--- cruil/src/device_kind.rs
+++ cruil/src/device_kind.rs
@@ -11,51 +11,17 @@ use DeviceKind::*;
 use hidparser::ReportField;
 
 impl DeviceKind {
-    #[doc = include_str!("device_kind_from_info.md")]
-    #[cfg(target_os = "macos")]
+    /// Tries to figure out what kind of device the [`DeviceInfo`] is describing.
+    /// This is achieved through looking at its Usage info.
+    ///
+    /// If the type of device is unsupported (not a [Mouse] or [Keyboard]),
+    /// this function returns [`None`].
+    #[cfg(not(any(target_os = "windows", target_os = "linux")))]
     pub fn from_info(info: &DeviceInfo) -> CruilResult<DeviceKind> {
         match (info.usage_page(), info.usage()) {
             (1, 6) => Ok(Keyboard),
             (1, 2) => Ok(Mouse),
             (p, u) => Err(crate::CruilError::UnsupportedDeviceKind(p, u)),
         }
-    }
-
-    #[doc = include_str!("device_kind_from_info.md")]
-    #[cfg(not(target_os = "macos"))]
-    pub fn from_info(info: &DeviceInfo) -> CruilResult<DeviceKind> {
-        // TEMPORARY HACK: Remove this when refactoring (hopefully soon)
-        // This API will hopefully become private anyway and the type of device will be provided by the library
-        // Also something something multiple backends
-        // This must not end up in a release in this state!
-        let hid = HidApi::new()?;
-        let device = info.open_device(&hid)?;
-        let mut buffer = [0; MAX_REPORT_DESCRIPTOR_SIZE];
-        let report_descriptor_size = device.get_report_descriptor(&mut buffer)?;
-        let report_descriptor = &buffer[..report_descriptor_size];
-        let report_descriptor = hidparser::parse_report_descriptor(report_descriptor).expect(
-            "Error in temporary code that should not be relied upon or end up in a release",
-        );
-        // This is a dirty dirty evil hack that does not produce accurate results
-        // I need to test if any of this works at all though
-        report_descriptor
-            .input_reports
-            .iter()
-            .find_map(|r| {
-                r.fields.iter().find_map(|f| {
-                    if let ReportField::Variable(v) = f {
-                        v.member_of
-                            .iter()
-                            .find_map(|c| match (c.usage.page(), c.usage.id()) {
-                                (1, 6) => Some(Keyboard),
-                                (1, 2) => Some(Mouse),
-                                _ => None,
-                            })
-                    } else {
-                        None
-                    }
-                })
-            })
-            .ok_or(crate::CruilError::UnsupportedDeviceKind(0, 0))
     }
 }
